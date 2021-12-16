727970e48a46d2395fd610a2cdd08e218937fd4e
Diego Canchari <cancharimmd@gmail.com>
2021-12-15 21:13:46 -0500

    Versión Final

--- Proyecto_Final/AppCoder/models.py
+++ Proyecto_Final/AppCoder/models.py
@@ -7,6 +7,8 @@ class Autos(models.Model):
     tipo = models.CharField(max_length=40)
     entregado = models.BooleanField()
 
+    def __str__(self):
+        return f"AUTO: Marca {self.marca} - Modelo {self.modelo} - Tipo {self.tipo} - ¿Entregado? {self.entregado}"
 
 class Inmuebles(models.Model):
 
@@ -14,9 +16,14 @@ class Inmuebles(models.Model):
     ciudad = models.CharField(max_length=40)
     anio = models.IntegerField()
 
+    def __str__(self):
+        return f"INMUEBLE: Dirección {self.direccion} - Ciudad {self.ciudad} - Año {self.anio}"
 class Facultad(models.Model):
 
     anio = models.IntegerField()
     carrera = models.CharField(max_length=40)
     universidad = models.CharField(max_length=40)
     email = models.EmailField(max_length=40, default="", editable = False)
+ 
+    def __str__(self):
+        return f"FACULTAD: Año {self.anio} - Carrera {self.carrera} - Universidad {self.universidad} - Email {self.email}"
